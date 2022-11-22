44d56d276a4a7e41c07645815d4d950b7a63b069
Ris1103 <rishankgautam8@gmail.com>
2022-11-22 16:45:48 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,1 +1,27 @@
 # this is for api "Backend"
+def waitingTime(process, wt, quantum):
+	n=len(process)
+	rt=[0]*n
+
+	for i in range(n):
+		rt[i]=process[i][2]
+
+	current_t=0
+
+	while(1):
+		flag=True	
+
+		for i in range(n):
+			if rt[i]>0:
+				flag=False
+
+				if rt[i]>quantum:
+					current_t+=quantum
+					rt[i]-=quantum
+
+				current_t+=rt[i]
+				wt[i]=current_t-process[i][2]
+				rt[i]=0
+
+		if flag==True:
+			break
